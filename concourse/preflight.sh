92cfec3b26814f3db3b20b59804ff8439aaa0c39
Marco Valtas <mavcunha@gmail.com>
2018-03-13 11:15:47 -0300

    First try to add a new pipeline

--- concourse/preflight.sh
+++ concourse/preflight.sh
@@ -6,8 +6,6 @@ FLY_BIN="${ROOT_DIR}/fly"
 CONCOURSE_EXTERNAL_URL="http://10.0.1.2:8080"
 export CONCOURSE_EXTERNAL_URL
 
-${FLY_BIN} targets
-
 ${FLY_BIN} -t lite login --team-name=main --username=concourse --password=changeme --concourse-url=${CONCOURSE_EXTERNAL_URL}
 
-${FLY_BIN} -t lite pipelines
+[[ -f mavcunha-misc.yml ]] && ${FLY_BIN} -t lite set-pipeline mavcunha-misc -c mavcunha-misc.yml
