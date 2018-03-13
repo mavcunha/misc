8162191380b005cf19ae30f80f41b38d5f2072a5
Marco Valtas <mavcunha@gmail.com>
2018-03-13 11:19:17 -0300

    More verbose and correct path configuration file

--- concourse/preflight.sh
+++ concourse/preflight.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd)"
 FLY_BIN="${ROOT_DIR}/fly"
 
@@ -8,4 +10,4 @@ export CONCOURSE_EXTERNAL_URL
 
 ${FLY_BIN} -t lite login --team-name=main --username=concourse --password=changeme --concourse-url=${CONCOURSE_EXTERNAL_URL}
 
-[[ -f mavcunha-misc.yml ]] && ${FLY_BIN} -t lite set-pipeline mavcunha-misc -c mavcunha-misc.yml
+${FLY_BIN} -t lite set-pipeline mavcunha-misc -c ${ROOT_DIR}/mavcunha-misc.yml
