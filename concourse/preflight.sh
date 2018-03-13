f26d43fc79d888d1a38c81d5c7e96022927c9ad8
Marco Valtas <mavcunha@gmail.com>
2018-03-13 10:45:30 -0300

    Fixed arg order and use extended version for clarification

--- concourse/preflight.sh
+++ concourse/preflight.sh
@@ -3,6 +3,6 @@
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd)"
 FLY_BIN=${ROOT_DIR}/fly
 
-${FLY_BIN} -t local -n main -u concourse -p changeme login
+${FLY_BIN} -t local login --team-name=main --username=concourse --password=changeme
 
 ${FLY_BIN} -t local pipelines
