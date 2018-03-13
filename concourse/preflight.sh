baed3caa3b671d391c8edf634fe210a3d4375f65
Marco Valtas <mavcunha@gmail.com>
2018-03-13 10:51:29 -0300

    List targets since we can't find them

--- concourse/preflight.sh
+++ concourse/preflight.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd)"
-FLY_BIN=${ROOT_DIR}/fly
+FLY_BIN="${ROOT_DIR}/fly"
 
-${FLY_BIN} -t lite login --team-name=main --username=concourse --password=changeme
+${FLY_BIN} targets
+
+${FLY_BIN} -t lite login --team-name=main --username=concourse --password=changeme --concourse-url=localhost:8080
 
 ${FLY_BIN} -t lite pipelines
